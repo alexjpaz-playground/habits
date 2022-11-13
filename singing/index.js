d595d03810b1bb245df0a427bace19bda625025d
Alexander Paz <alexjpaz@gmail.com>
2022-11-12 20:16:30 -0500

    singing pitch

--- singing/index.js
+++ singing/index.js
@@ -44,7 +44,39 @@ function Frame() {
       {
         name: 'Bubble Cool Down on Reverse Single Arpeggio - Low' ,
         audioUrl: "./media/Bubble_Cooldown.mp3",
-      }
+      },
+      {
+        name: "Ugly Ah - Descending 5 Tone Repeat (Low Voices)",
+        audioUrl: "./media/Ugly_Ah_Low.mp3"
+      },
+      {
+        name: "Ugly Ah - Descending 5 Tone Repeat (High Voices)",
+        audioUrl: "./media/Ugly_Ah_High.mp3"
+      },
+      {
+        name: "Mmm - 5 Tone Scale (Low Voices)",
+        audioUrl: "./media/Mmm_Low.mp3"
+      },
+      {
+        name: "Mmm 5 Tone Scale (High Voices)",
+        audioUrl: "./media/Mmm_High.mp3"
+      },
+      {
+        name: "Goo-Koo Descending Arpeggio (Low Voices)",
+        audioUrl: "./media/Goo-Koo_Low.mp3"
+      },
+      {
+        name: "Goo-Koo Descending Arpeggio (High Voices)",
+        audioUrl: "./media/Goo-Koo_High.mp3"
+      },
+      {
+        name: "Gug Descending Arpeggio (Low Voices)",
+        audioUrl: "./media/Goo-Koo_Low.mp3"
+      },
+      {
+        name: "Gug Descending Arpeggio (High Voices)",
+        audioUrl: "./media/Goo-Koo_High.mp3"
+      },
     ]
   };
 
