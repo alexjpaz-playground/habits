3932a790bd0d49f9361a7b0a84c41a2b64ab6c21
Alexander Paz <alexjpaz@gmail.com>
2022-11-18 15:52:47 -0500

    Adding drum links

---
+++ src/drums/data.js
@@ -0,0 +1,17 @@
+
+const data = {};
+
+data.rudiments = [
+    {
+        title: "01: SINGLE STROKE ROLL",
+        titleUrl: "https://vicfirth.zildjian.com/education/01-single-stroke-roll.html",
+        videoUrl: "https://www.youtube.com/embed/Fj0SeWV5d_c?enablejsapi=1&amp;rel=0"
+    },
+    {
+        title: "02: SINGLE STROKE FOUR",
+        titleUrl: "https://vicfirth.zildjian.com/education/02-single-stroke-four.html",
+        videoUrl: "https://www.youtube.com/embed/r66CkN-VjzE?enablejsapi=1&amp;rel=0"
+    }
+];
+
+export default data;
--- src/drums/index.js
+++ src/drums/index.js
@@ -1,6 +1,37 @@
 import React from "react";
 
 import { Breadcrumbs } from "./Breadcrumbs";
+
+import data from "./data";
+
+export function useData() {
+    return data;
+}
+
+export function RudimentsCard(props) {
+
+
+    return (
+        <div className="">
+            <h2 className="title"><a href={props.titleUrl}>{props.title}</a></h2>
+            <div style={{"height":"20rem"}}>
+                <iframe title={props.title} width="100%" height="100%" src={props.videoUrl} allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen=""></iframe>
+            </div>
+      </div>
+    );
+}
+
+export function RudimentsCardSet() {
+    const data = useData();
+
+    return (
+      <div className="">
+        { data.rudiments.map((rudiment) => (
+            <RudimentsCard {...rudiment} />
+        )) }
+      </div>
+    );
+}
   
 export default function Drums() {
     return (
@@ -11,8 +42,14 @@ export default function Drums() {
           </h1>
           <Breadcrumbs />
           <hr />
-          <a className="button is-large" href='https://trello.com/c/ZEF8DZaV/137-learn-to-drum'>Rudiments</a>
+          <div className="buttons">
+             <a className="button is-large" href='https://trello.com/c/ZEF8DZaV/137-learn-to-drum'>Trello Board</a>
+             <a className="button is-large" href='https://vicfirth.zildjian.com/education/40-essential-rudiments.html'>Rudiments (vic)</a>
+             <a className="button is-large" href='https://www.40drumrudiments.com/'>Rudiments (vic)</a>
+          </div>
+          <hr />
+          <RudimentsCardSet />
         </div>
       </section>
-    )
+    );
 }
