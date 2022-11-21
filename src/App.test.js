2f2bfa0bba5cc6ad0909ce26b38da104ccb15308
Alexander Paz <alexjpaz@gmail.com>
2022-11-20 20:31:39 -0500

    Fixing test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Habits/i);
+  const linkElement = screen.getByTestId('Home');
   expect(linkElement).toBeInTheDocument();
 });
--- src/Home.js
+++ src/Home.js
@@ -16,13 +16,13 @@ function Frame({ children }) {
 export default function Home() {
     return (
         <Frame>
-            <h1 className="title">Habits</h1>
+            <h1 className="title" data-testid="Home">Habits</h1>
             <div className="buttons">
-              <Link to="singing" class="button is-large is-fullwidth">Singing</Link>
-              <Link to="spanish" class="button is-large is-fullwidth">Spanish</Link>
-              <Link to="journaling" class="button is-large is-fullwidth">Journaling</Link>
-              <Link to="drums" class="button is-large is-fullwidth">Drums</Link>
-              <Link to="music" disabled class="button is-large is-fullwidth">Music</Link>
+              <Link to="singing" className="button is-large is-fullwidth">Singing</Link>
+              <Link to="spanish" className="button is-large is-fullwidth">Spanish</Link>
+              <Link to="journaling" className="button is-large is-fullwidth">Journaling</Link>
+              <Link to="drums" className="button is-large is-fullwidth">Drums</Link>
+              <Link to="music" disabled className="button is-large is-fullwidth">Music</Link>
             </div>
         </Frame>
     );
