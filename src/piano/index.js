7f5b1a6846ae39f41cd14a0b0ac520b53c70833d
Alexander Paz <alexjpaz@gmail.com>
2022-11-23 13:14:12 -0500

    fixing alt tag

--- src/piano/index.js
+++ src/piano/index.js
@@ -14,7 +14,7 @@ export function PianoHabit() {
           <div className='buttons'>
             <a href='https://www.youtube.com/watch?v=PzQY2K5Ormc&t=46s' className='button'>C Major Arrpeggios</a>
           </div>
-          <img src='https://trello.com/1/cards/635930b17bdb4300e08b15c8/attachments/635930dca34f9801ac04cabd/previews/635930dda34f9801ac04caf4/download/Screenshot_20221026-090617.png' />
+          <img alt='minor triads' src='https://trello.com/1/cards/635930b17bdb4300e08b15c8/attachments/635930dca34f9801ac04cabd/previews/635930dda34f9801ac04caf4/download/Screenshot_20221026-090617.png' />
           <hr />
           <Rewards />
         </div>
